cb076f465e274b8fa0a4f27d6c5fc6c366779233
yuik23 <yui_karamatsu@yahoo.co.jp>
2023-02-25 05:43:02 +0900

    時間ごとに表示する挨拶を変更

--- src/main/java/com/example/javatask6/HelloWorldController.java
+++ src/main/java/com/example/javatask6/HelloWorldController.java
@@ -3,10 +3,22 @@ package com.example.javatask6;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class HelloWorldController {
   @GetMapping("/hello")
-  public String hello(){
-    return "hello world";
+  public String hello() {
+    LocalDateTime now = LocalDateTime.now();
+    if (3 < now.getHour() && now.getHour() < 12) {
+      return "おはようございます！今日も１日がんばろー！";
+    } else if (now.getHour() < 16) {
+      return "こんにちは！";
+    } else {
+      return "こんばんは！今日も１日おつかれさまです！";
+    }
   }
 }
+
+
+
