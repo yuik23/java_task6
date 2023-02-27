100a876d21e3ce0a202fd9893fcb7f6cb4fd64fa
yuik23 <yui_karamatsu@yahoo.co.jp>
2023-02-28 05:47:50 +0900

    時刻が日本時刻であることを明記

--- src/main/java/com/example/javatask6/HelloWorldController.java
+++ src/main/java/com/example/javatask6/HelloWorldController.java
@@ -4,18 +4,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 
 @RestController
 public class HelloWorldController {
   @GetMapping("/hello")
   public String hello() {
-    LocalDateTime now = LocalDateTime.now();
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
+    //日本時間を取得
+    LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Tokyo"));
+
     if (3 < now.getHour() && now.getHour() < 12) {
-      return "おはようございます！今日も１日がんばろー！";
+      return "<ただいま日本時刻" + now.format(dtf) + "> おはようございます！今日も１日がんばろー！";
     } else if (now.getHour() < 16) {
-      return "こんにちは！";
+      return "<ただいま日本時刻" + now.format(dtf) + "> こんにちは！";
     } else {
-      return "こんばんは！今日も１日おつかれさまです！";
+      return "<ただいま日本時刻" + now.format(dtf) + "> こんばんは！今日も１日おつかれさまです！";
     }
   }
 }
